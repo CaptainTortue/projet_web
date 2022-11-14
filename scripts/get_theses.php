5e1f2725ea709bf3522af9d19ae987ad558702eb
CaptainTortue <tristan.martinez@sfr.fr>
2022-11-14 22:41:08 +0100

    debut front

--- scripts/get_theses.php
+++ scripts/get_theses.php
@@ -1,66 +1,40 @@
 <?php
-session_start();
- //initialisation des variable
 
 function get_these($cnx) {
     if (isset($_SESSION["login"])){ 
         echo 'ihr';
-        $request ="SELECT DISTINCT theses.id, theses.etablissements_soutenance";
-        if ((isset($_POST["nom"]) && $_POST["nom"]!="")||isset($_POST["prenom"]) && $_POST["prenom"]!="") {
-            $request .= ", personnes.nom, personnes.prenom ";
-        }
-        
-        if (isset($_POST["titre"]) && $_POST["titre"]!="") {
-            $request .= ", titres.titre ";
-        }
-        $request .= "FROM theses";
-        /*
-        if (isset($_POST["nom"])) {
-            $personneNom = $cnx->prepare("SELECT id FROM personnes WHERE nom=:nom");        
-            $personneNom->bindParam(':nom', $_POST["nom"], PDO::PARAM_STR);
-        }*/
-        /*
-        if (isset($_POST["prenom"])) {
-            $personnePrenom = $cnx->prepare("SELECT id FROM personnes WHERE prenom=:prenom");        
-            $personnePrenom->bindParam(':prenom', $_POST["prenom"], PDO::PARAM_STR);
-        }*/
-        /*
-        if (isset($_POST["titre"])) {
-            $personnePrenom = $cnx->prepare("SELECT * FROM these WHERE titre LIKE '%:titre%'");        
-            $personnePrenom->bindParam(':titre', $_POST["titre"], PDO::PARAM_STR);
-        }*/
-        if ((isset($_POST["nom"]) && $_POST["nom"]!="")||(isset($_POST["prenom"]) && $_POST["prenom"]!="")||(isset($_POST["titre"]) && $_POST["titre"]!="")) {
-            $request .= ", titres, personnes, auteurs WHERE
-            (auteurs.idPersonne = personnes.id AND auteurs.idThese = theses.id)
-            ";
-        }
+        $request ="SELECT DISTINCT theses.id, theses.etablissements_soutenance, theses.date_soutenance, theses.status, theses.discipline, theses.langue, theses.nnt, resumes.resume, titres.titre, personnes.nom, personnes.prenom ";
+
+        $request .= " FROM theses LEFT JOIN resumes ON theses.id = resumes.idThese LEFT JOIN titres ON theses.id = titres.idThese, personnes, auteurs WHERE
+        (auteurs.idPersonne = personnes.id AND auteurs.idThese = theses.id) ";
+
         if (isset($_POST["nom"]) && $_POST["nom"]!="") {
-            echo ("nom");
-            $request .= " AND personnes.nom = :nom";
+            $nom = "%".$_POST["nom"]."%";
+            $request .= " AND personnes.nom LIKE :nom";
         }
         if (isset($_POST["prenom"]) && $_POST["prenom"]!="") {
-            echo ("prenom");
-            $request .= " AND personnes.prenom = :prenom";
+            $prenom = "%".$_POST["prenom"]."%";
+            $request .= " AND personnes.prenom LIKE :prenom";
         }
         if (isset($_POST["titre"]) && $_POST["titre"]!="") {
             echo ("titre");
             $titre = "%".$_POST["titre"]."%";
-            $request .= " AND titres.titre LIKE :titre AND titres.idThese = theses.id";
+            $request .= " AND titres.titre LIKE :titre";
         }
         $theses = $cnx->prepare($request);
         if (isset($_POST["prenom"]) && $_POST["prenom"]!="") {
             echo ("prenom");
-            $theses->bindParam(':prenom', $_POST["prenom"], PDO::PARAM_STR);  
+            $theses->bindParam(':prenom', $prenom, PDO::PARAM_STR);  
         } 
         if (isset($_POST["nom"]) && $_POST["nom"]!="") {
             echo ("nom");
-            $theses->bindParam(':nom', $_POST["nom"], PDO::PARAM_STR);  
+            $theses->bindParam(':nom', $nom, PDO::PARAM_STR);  
         }
         if (isset($_POST["titre"]) && $_POST["titre"]!="") {
             echo ("titre");
             $theses->bindParam(':titre', $titre, PDO::PARAM_STR);  
         }
-        $theses->debugDumpParams();
+        //$theses->debugDumpParams();
         $theses->execute();
         print_r($theses->errorInfo());
         return $theses->fetchAll();
